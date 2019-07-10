7a4136d2efa567463e2d600f7e5c52e2c08c1a09
Charles Ferentchak <ferentchak@gmail.com>
2019-07-09 22:24:34 -0500

    builds

--- blocker-resolution-time-grid/Gruntfile.js
+++ blocker-resolution-time-grid/Gruntfile.js
@@ -1,48 +1,50 @@
+/* eslint-disable */
+
 module.exports = function(grunt) {
     require('grunt');
-    
+
     var config_file_name = 'config.json';
     var auth_file_name = 'auth.json';
-    
+
     var config = { auth: {} };
-    
-    if ( grunt.file.exists(config_file_name) ) {
-    
+
+    if (grunt.file.exists(config_file_name)) {
+
         config = grunt.file.readJSON('config.json');
 
-        config.js_files = grunt.file.expand(['../utils/src/javascript/**/*.js','src/javascript/*.js']);
+        config.js_files = grunt.file.expand(['../utils/src/javascript/**/*.js', 'src/javascript/*.js']);
 
         config.ugly_files = grunt.file.expand(['deploy/app.min.*.js']);
-        
-        config.css_files = grunt.file.expand( 'src/style/*.css' );
+
+        config.css_files = grunt.file.expand('src/style/*.css');
         config.checksum = "<!= checksum !>";
-        
+
         config.js_contents = " ";
-        for (var i=0;i<config.js_files.length;i++) {
-            grunt.log.writeln( config.js_files[i]);
+        for (var i = 0; i < config.js_files.length; i++) {
+            grunt.log.writeln(config.js_files[i]);
             config.js_contents = config.js_contents + "\n" + grunt.file.read(config.js_files[i]);
         }
-    
+
         config.style_contents = "";
-        for (var i=0;i<config.css_files.length;i++) {
-            grunt.log.writeln( config.css_files[i]);
+        for (var i = 0; i < config.css_files.length; i++) {
+            grunt.log.writeln(config.css_files[i]);
             config.style_contents = config.style_contents + "\n" + grunt.file.read(config.css_files[i]);
         }
-        
+
         config.ugly_contents = "";
-        for ( var i=0;i<config.ugly_files;i++ ) {
+        for (var i = 0; i < config.ugly_files; i++) {
             grunt.file.read(config.ugly_files[i]);
         }
     }
-    if ( grunt.file.exists(auth_file_name) ) {
-    // grunt.log.writeln( config.js_contents );
+    if (grunt.file.exists(auth_file_name)) {
+        // grunt.log.writeln( config.js_contents );
         var auth = grunt.file.readJSON(auth_file_name);
         config.auth = auth
     } else {
         grunt.log.writeln("");
         grunt.log.writeln("WARNING: Slow tests won't run without an auth.json file");
     }
-    
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
@@ -54,30 +56,30 @@ module.exports = function(grunt) {
             }
         },
         template: {
-                dev: {
-                    src: 'templates/App-debug-tpl.html',
-                    dest: 'App-debug.html',
-                    engine: 'underscore',
-                    variables: config
-                },
-                prod: {
-                    src: 'templates/App-tpl.html',
-                    dest: 'deploy/App.txt',
-                    engine: 'underscore',
-                    variables: config
-                },
-                apikey: {
-                    src: 'templates/App-apikey-tpl.html',
-                    dest: 'deploy/ExternalApp.html',
-                    engine: 'underscore',
-                    variables: config
-                },
-                ugly: {
-                    src: 'templates/App-ugly-tpl.html',
-                    dest: 'deploy/Ugly.txt',
-                    engine: 'underscore',
-                    variables: config
-                }
+            dev: {
+                src: 'templates/App-debug-tpl.html',
+                dest: 'App-debug.html',
+                engine: 'underscore',
+                variables: config
+            },
+            prod: {
+                src: 'templates/App-tpl.html',
+                dest: 'deploy/App.txt',
+                engine: 'underscore',
+                variables: config
+            },
+            apikey: {
+                src: 'templates/App-apikey-tpl.html',
+                dest: 'deploy/ExternalApp.html',
+                engine: 'underscore',
+                variables: config
+            },
+            ugly: {
+                src: 'templates/App-ugly-tpl.html',
+                dest: 'deploy/Ugly.txt',
+                engine: 'underscore',
+                variables: config
+            }
         },
         jasmine: {
             fast: {
@@ -88,7 +90,7 @@ module.exports = function(grunt) {
                     template: 'test/fast/custom.tmpl',
                     templateOptions: config,
                     keepRunner: true,
-                    junit: { 
+                    junit: {
                         path: 'test/logs/fast'
                     }
                 }
@@ -102,14 +104,14 @@ module.exports = function(grunt) {
                     templateOptions: config,
                     keepRunner: true,
                     timeout: 50000,
-                    junit: { 
+                    junit: {
                         path: 'test/logs/slow'
                     }
                 }
             }
         }
     });
-    
+
     grunt.registerTask('setChecksum', 'Make a sloppy checksum', function() {
         var fs = require('fs');
         var chk = 0x12345678,
@@ -117,20 +119,20 @@ module.exports = function(grunt) {
         var deploy_file = 'deploy/App.txt';
 
         var file = grunt.file.read(deploy_file);
-        string = file.replace(/var CHECKSUM = .*;/,"");
-        string = string.replace(/\s/g,"");  //Remove all whitespace from the string.
-        
+        string = file.replace(/var CHECKSUM = .*;/, "");
+        string = string.replace(/\s/g, ""); //Remove all whitespace from the string.
+
         for (i = 0; i < string.length; i++) {
             chk += (string.charCodeAt(i) * i);
         }
         grunt.log.writeln('sloppy checksum: ' + chk);
         grunt.log.writeln('length: ' + string.length);
-// 
-        grunt.template.addDelimiters('square-brackets','[%','%]');
-        
-        var output = grunt.template.process(file, { data: { checksum: chk },  delimiters: 'square-brackets' });
-        grunt.file.write(deploy_file,output);
-        
+        // 
+        grunt.template.addDelimiters('square-brackets', '[%', '%]');
+
+        var output = grunt.template.process(file, { data: { checksum: chk }, delimiters: 'square-brackets' });
+        grunt.file.write(deploy_file, output);
+
     });
 
     //load
@@ -138,14 +140,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     //tasks
-    grunt.registerTask('default', ['debug','build','ugly','apikey']);
-    
+    grunt.registerTask('default', ['debug', 'build', 'apikey']);
+
     // (uses all the files in src/javascript)
-    grunt.registerTask('build', "Create the html for deployment",['template:prod','setChecksum']);
+    grunt.registerTask('build', "Create the html for deployment", ['template:prod', 'setChecksum']);
     // 
     grunt.registerTask('debug', "Create an html file that can run in its own tab", ['template:dev']);
     //
-    grunt.registerTask('ugly', "Create the ugly html for deployment",['uglify:ugly','template:ugly']);
+    grunt.registerTask('ugly', "Create the ugly html for deployment", ['uglify:ugly', 'template:ugly']);
     //
     grunt.registerTask('apikey', "Create an html file that can run on another server", ['template:apikey']);
 
